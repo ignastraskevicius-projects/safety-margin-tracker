1df5f24b042409bae51ab5f5079e85cd08029e68
Ignas Traskevicius <ignas.traskevicius@gmail.com>
2022-01-27 18:20:22 +0100

    setting up basic spring boot app

---
+++ src/main/java/org/ignast/investing/safetymargin/api/TestSpringApp.java
@@ -0,0 +1,11 @@
+package org.ignast.investing.safetymargin.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringApp {
+    public static void main(String[] args) {
+        SpringApplication.run(TestSpringApp.class, args);
+    }
+}

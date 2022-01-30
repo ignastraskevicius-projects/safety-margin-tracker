5695cc3cfbee26f81963c3510aa2ac7329401720
Ignas Traskevicius <ignas.traskevicius@gmail.com>
2022-01-30 11:25:40 +0100

    root resource links to companies

--- src/main/java/org/ignast/stockinvesting/api/RootController.java
+++ src/main/java/org/ignast/stockinvesting/api/RootController.java
@@ -1,21 +1,21 @@
 package org.ignast.stockinvesting.api;
 
+import org.springframework.hateoas.LinkRelation;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
-public class TestController {
+public class RootController {
 
-    @RequestMapping("/greeting")
-    public HttpEntity<Greeting> greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        Greeting greeting = new Greeting(String.format("hello, %s", name));
-        greeting.add(linkTo(methodOn(TestController.class).greeting(name)).withSelfRel());
-        return new ResponseEntity<>(greeting, HttpStatus.OK);
+    @RequestMapping("/")
+    public HttpEntity<Root> root() {
+        Root root = new Root();
+        root.add(linkTo(methodOn(RootController.class).root()).withRel("stocks:company"));
+        return new ResponseEntity<>(root, HttpStatus.BAD_REQUEST);
     }
 }

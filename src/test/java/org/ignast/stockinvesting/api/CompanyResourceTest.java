7c1ce0f6d73832ba66c5a03e705eb4c6b80652b9
Ignas Traskevicius <ignas.traskevicius@gmail.com>
2022-02-02 15:49:00 +0100

    rootResource rejects unversioned and non HAL requests

--- src/test/java/org/ignast/stockinvesting/api/CompanyResourceTest.java
+++ src/test/java/org/ignast/stockinvesting/api/CompanyResourceTest.java
@@ -8,13 +8,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.*;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.ignast.stockinvesting.api.Uris.rootResourceOn;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-public class CompanyTest {
+public class CompanyResourceTest {
     @LocalServerPort
     private int port;
 
@@ -23,16 +23,19 @@ public class CompanyTest {
 
     @Test
     public void shouldDefineCompany() throws JSONException {
-        String url = "http://localhost:" + port + "/";
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Accept", "application/vnd.stockinvesting.estimates-v1.hal+json");
-        ResponseEntity<String> rootResponse = restTemplate.exchange(url, HttpMethod.GET, new HttpEntity<>(headers), String.class);
+        ResponseEntity<String> rootResponse = restTemplate.exchange(rootResourceOn(port), HttpMethod.GET, v1(), String.class);
         assertThat(rootResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
         JSONObject root = new JSONObject(rootResponse.getBody());
 
         String companiesHref = root.getJSONObject("_links").getJSONObject("stocks:company").getString("href");
         ResponseEntity<String> companyDefinition = restTemplate.postForEntity(companiesHref, "", String.class);
         assertThat(companyDefinition.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+    }
+
+    private HttpEntity<String> v1() {
+        MediaType v1MediaType = MediaType.valueOf("application/vnd.stockinvesting.estimates-v1.hal+json");
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Accept", v1MediaType.toString());
+        return new HttpEntity<>(headers);
     }
 }
--- src/test/java/org/ignast/stockinvesting/api/RootControllerTest.java
+++ src/test/java/org/ignast/stockinvesting/api/RootControllerTest.java
@@ -1,42 +1,26 @@
 package org.ignast.stockinvesting.api;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.StringDescription;
-import org.hamcrest.TypeSafeMatcher;
-import org.ignast.stockinvesting.api.test.HateoasJsonMatchers;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.ResultMatcher;
-
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.ignast.stockinvesting.api.test.HateoasJsonMatchers.hasRel;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(RootController.class)
 public class RootControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+    private @Autowired MockMvc mockMvc;
+
+    private String V1_MEDIA_TYPE = "application/vnd.stockinvesting.estimates-v1.hal+json";
 
     @Test
-    public void rootShouldContainLinkToCompanies() throws Exception {
-        ResultActions root = mockMvc.perform(get("/"));
+    public void rootResourceShouldLinkToCompanies() throws Exception {
+        ResultActions root = mockMvc.perform(get("/").accept(V1_MEDIA_TYPE));
         root.andExpect(status().isOk()).andExpect(content().string(hasRel("stocks:company").withHrefContaining("/companies")));
-    }
-
-    @Test
-    public void shouldAcceptOnlyGetMethodRequests() throws Exception {
-        ResultActions post = mockMvc.perform(post("/"));
-        post.andExpect(status().isMethodNotAllowed());
     }
 }
 

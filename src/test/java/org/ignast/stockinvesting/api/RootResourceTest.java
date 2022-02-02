a9553ee730578ad9a68b0039ae10e0b4ac4ed6ab
Ignas Traskevicius <ignas.traskevicius@gmail.com>
2022-02-02 15:50:46 +0100

    GET requests without Accept header are rejected

--- src/test/java/org/ignast/stockinvesting/api/RootResourceTest.java
+++ src/test/java/org/ignast/stockinvesting/api/RootResourceTest.java
@@ -7,6 +7,11 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.*;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.ignast.stockinvesting.api.Uris.rootResourceOn;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
@@ -37,6 +42,15 @@ public class RootResourceTest {
 
         ResponseEntity<String> rootResponse = restTemplate.exchange(rootResourceOn(port), HttpMethod.GET, request, String.class);
         assertThat(rootResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_ACCEPTABLE);
+    }
+
+    @Test
+    public void shouldRejectWithNoAcceptHeader() throws Exception {
+        HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create(rootResourceOn(port))).build();
+
+        HttpClient client = HttpClient.newBuilder().build();
+        HttpResponse<String> rootResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+        assertThat(rootResponse.statusCode()).isEqualTo(HttpStatus.NOT_ACCEPTABLE.value());
     }
 
     private HttpEntity<String> v1() {

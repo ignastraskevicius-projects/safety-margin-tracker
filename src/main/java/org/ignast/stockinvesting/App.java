ffdabbe7083f7005128d6961b067ab7d57fc1e1e
Ignas Traskevicius <ignas.traskevicius@gmail.com>
2022-02-06 12:52:03 +0100

    moving a Main App file to start the app out of API package

--- src/main/java/org/ignast/stockinvesting/App.java
+++ src/main/java/org/ignast/stockinvesting/App.java
@@ -1,4 +1,4 @@
-package org.ignast.stockinvesting.api;
+package org.ignast.stockinvesting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

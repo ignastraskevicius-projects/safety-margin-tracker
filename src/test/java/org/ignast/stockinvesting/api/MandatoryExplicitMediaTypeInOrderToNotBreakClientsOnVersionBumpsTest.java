ccc23f0ca90e4f23c6895a4a5df09aa8aaa2634d
Ignas Traskevicius <ignas.traskevicius@gmail.com>
2022-02-06 10:28:16 +0100

    unacceptable rootResource mediaTypes tests brought to unit test level

---
+++ src/test/java/org/ignast/stockinvesting/api/MandatoryExplicitMediaTypeInOrderToNotBreakClientsOnVersionBumpsTest.java
@@ -0,0 +1,30 @@
+package org.ignast.stockinvesting.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.ignast.stockinvesting.api.Uris.rootResourceOn;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class MandatoryExplicitMediaTypeInOrderToNotBreakClientsOnVersionBumpsTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Test
+    public void GETRequestsMustHaveAcceptHeader() throws Exception {
+        HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create(rootResourceOn(port))).build();
+
+        HttpClient client = HttpClient.newBuilder().build();
+        HttpResponse<String> rootResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
+        assertThat(rootResponse.statusCode()).isEqualTo(HttpStatus.NOT_ACCEPTABLE.value());
+    }
+}

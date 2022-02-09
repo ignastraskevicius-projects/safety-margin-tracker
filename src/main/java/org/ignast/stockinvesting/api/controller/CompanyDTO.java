70400eef6b6de63c98741577e66b7b8d01eefa95
Ignas Traskevicius <ignas.traskevicius@gmail.com>
2022-02-09 09:59:19 +0100

    company creation is rejected when address submitted is a primitive type

--- src/main/java/org/ignast/stockinvesting/api/controller/CompanyDTO.java
+++ src/main/java/org/ignast/stockinvesting/api/controller/CompanyDTO.java
@@ -2,8 +2,16 @@ package org.ignast.stockinvesting.api.controller;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
 public class CompanyDTO {
+
+    @NotNull
+    private final List<CompanyDTO> address;
+
     public CompanyDTO(@JsonProperty(value = "name", required = true) String name,
-            @JsonProperty(value = "address", required = true) String address) {
+            @JsonProperty(value = "address", required = true) List<CompanyDTO> address) {
+        this.address = address;
     }
 }

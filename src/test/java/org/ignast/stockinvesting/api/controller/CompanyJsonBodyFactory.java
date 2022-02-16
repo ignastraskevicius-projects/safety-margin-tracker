3852ecc6b6361a0ea5c1dfb9b6232dc87093dd89
Ignas Traskevicius <ignas.traskevicius@gmail.com>
2022-02-16 09:30:27 +0100

    Controller tests excercising name and address now uses factory to construct json bodies

---
+++ src/test/java/org/ignast/stockinvesting/api/controller/CompanyJsonBodyFactory.java
@@ -0,0 +1,21 @@
+package org.ignast.stockinvesting.api.controller;
+
+public class CompanyJsonBodyFactory {
+    public String createAmazon() {
+        return "{\"name\":\"Amazon\",\"address\":{\"country\":\"United States\"},\"listings\":\"listings\"}";
+    }
+
+    public String createWithNameJsonPair(String nameJsonPair) {
+        return String.format("{%s\"address\":{\"country\":\"United States\"},\"listings\":\"listings\"}",
+                appendCommaIfNotEmpty(nameJsonPair));
+    }
+
+    public String createWithAddressJsonPair(String addressJsonPair) {
+        return String.format("{\"name\":\"Amazon\",%s\"listings\":\"listings\"}",
+                appendCommaIfNotEmpty(addressJsonPair));
+    }
+
+    private String appendCommaIfNotEmpty(String jsonPair) {
+        return jsonPair + (jsonPair.isEmpty() ? "" : ",");
+    }
+}

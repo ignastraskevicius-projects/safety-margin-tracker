a836b6e301907aba0181238dfbb181c7986d73d0
Ignas Traskevicius <ignas.traskevicius@gmail.com>
2022-02-16 13:06:41 +0100

    API does not support companies with multiple listings yet

--- src/test/java/org/ignast/stockinvesting/api/controller/CompanyControllerTest.java
+++ src/test/java/org/ignast/stockinvesting/api/controller/CompanyControllerTest.java
@@ -236,11 +236,19 @@ class CompanyControllerListingsParsingTest {
 
     @ParameterizedTest
     @ValueSource(strings = { "true", "false", "3", "3.3", "\"someString\", []" })
-    public void companyWithIndividualListingAsNonObjectShouldBeRejectedIndicatedWrongType(String listringOfWrongType)
+    public void companyWithIndividualListingAsNonObjectShouldBeRejectedIndicatedWrongType(String listingOfWrongType)
             throws Exception {
         mockMvc.perform(post("/companies/").contentType(V1_MEDIA_TYPE).content(
-                bodyFactory.createWithListingsJsonPair(String.format("\"listings\":[%s]", listringOfWrongType))))
+                bodyFactory.createWithListingsJsonPair(String.format("\"listings\":[%s]", listingOfWrongType))))
                 .andExpect(status().isBadRequest())
                 .andExpect(content().string("{\"errorName\":\"fieldMustBeObject\",\"jsonPath\":\"$.listings[0]\"}"));
+    }
+
+    @Test
+    public void shouldNotSupportMultipleListings() throws Exception {
+        mockMvc.perform(post("/companies/").contentType(V1_MEDIA_TYPE)
+                .content(bodyFactory.createWithListingsJsonPair("\"listings\":[{}, {}]")))
+                .andExpect(status().isBadRequest()).andExpect(content().string(
+                        "{\"errorName\":\"fieldHasInvalidValue\",\"jsonPath\":\"$.listings\",\"message\":\"Multiple listings are not supported\"}"));
     }
 }

c6589af3d54a84c70c443c3416b6d5577277b849
Ignas Traskevicius <ignas.traskevicius@gmail.com>
2022-02-17 16:34:33 +0100

    wrapping errors indicating missing json field into array, as multiple validation errors can occure at once

--- src/test/java/org/ignast/stockinvesting/api/controller/BodySchemaMismatchJsonErrors.java
+++ src/test/java/org/ignast/stockinvesting/api/controller/BodySchemaMismatchJsonErrors.java
@@ -2,6 +2,8 @@ package org.ignast.stockinvesting.api.controller;
 
 public class BodySchemaMismatchJsonErrors {
     public static String forMissingFieldAt(String jsonPath) {
-        return String.format("{\"errorName\":\"fieldIsMissing\",\"jsonPath\":\"%s\"}", jsonPath);
+        return String.format(
+                "{\"errorName\":\"bodyDoesNotMatchSchema\",\"validationErrors\":[{\"errorName\":\"fieldIsMissing\",\"jsonPath\":\"%s\"}]}",
+                jsonPath);
     }
 }

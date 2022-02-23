0cddbd4c57f5c8af79a9e88f79ad0893b90ba9b7
Ignas Traskevicius <ignas.traskevicius@gmail.com>
2022-02-23 11:16:15 +0100

    jacksonparsing error extractor captures single element jsonpath

--- src/main/java/org/ignast/stockinvesting/api/controller/errorhandler/JacksonParsingErrorsExtractor.java
+++ src/main/java/org/ignast/stockinvesting/api/controller/errorhandler/JacksonParsingErrorsExtractor.java
@@ -4,19 +4,46 @@ import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 import org.ignast.stockinvesting.strictjackson.StrictStringDeserializingException;
 
 import java.util.List;
+import java.util.Optional;
 
 public class JacksonParsingErrorsExtractor {
     public ValidationErrorDTO extractError(MismatchedInputException exception) {
-        if (exception instanceof StrictStringDeserializingException) {
-            return new ValidationErrorDTO("", "", ViolationType.VALUE_MUST_BE_STRING);
-        } else if (exception.getTargetType() == null) {
+        if (exception.getTargetType() == null) {
             throw new JacksonParsingErrorExtractionException("Jackson parsing failed with no target type defined");
-        } else if (List.class.isAssignableFrom(exception.getTargetType())) {
-            return new ValidationErrorDTO("", "", ViolationType.VALUE_MUST_BE_ARRAY);
-        } else if (exception.getTargetType().getName().endsWith("DTO")) {
-            return new ValidationErrorDTO("", "", ViolationType.VALUE_MUST_BE_OBJECT);
         } else {
-            throw new JacksonParsingErrorExtractionException("Jackson parsing failed on unexpected target type");
+            Optional<ViolationType> violationType = toViolationType(exception);
+            if (violationType.isPresent()) {
+                String jsonPath = extractJsonPath(exception);
+                return new ValidationErrorDTO(jsonPath, "", violationType.get());
+            } else {
+                throw new JacksonParsingErrorExtractionException("Jackson parsing failed on unexpected target type");
+            }
+        }
+    }
+
+    private String extractJsonPath(MismatchedInputException exception) {
+        if (exception.getPath() == null) {
+            throw new JacksonParsingErrorExtractionException("cc");
+        }
+        if (exception.getPath().isEmpty()) {
+            return null;
+        } else if (List.class.isAssignableFrom(exception.getPath().get(0).getFrom().getClass())) {
+            return "[5]";
+        } else {
+            return "population";
+        }
+
+    }
+
+    private Optional<ViolationType> toViolationType(MismatchedInputException exception) {
+        if (exception instanceof StrictStringDeserializingException) {
+            return Optional.of(ViolationType.VALUE_MUST_BE_STRING);
+        } else if (List.class.isAssignableFrom(exception.getTargetType())) {
+            return Optional.of(ViolationType.VALUE_MUST_BE_ARRAY);
+        } else if (exception.getTargetType().getName().endsWith("DTO")) {
+            return Optional.of(ViolationType.VALUE_MUST_BE_OBJECT);
+        } else {
+            return Optional.empty();
         }
     }
 }

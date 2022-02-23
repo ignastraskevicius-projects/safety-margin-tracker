f84d71261443b560b3b11904fc1306ca148944ce
Ignas Traskevicius <ignas.traskevicius@gmail.com>
2022-02-23 23:32:22 +0100

    unit test ensuring currency validator behaviour

---
+++ src/test/java/org/ignast/stockinvesting/api/controller/errorhandler/annotations/CurrencyValidatorTest.java
@@ -0,0 +1,23 @@
+package org.ignast.stockinvesting.api.controller.errorhandler.annotations;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CurrencyValidatorTest {
+    @Test
+    public void shouldNotBeValidForNullCurrencyCode() {
+        assertThat(new CurrencyValidator().isValid(null, null)).isFalse();
+    }
+
+    @Test
+    public void shouldNotBeValidForNonexistentCurrencyCode() {
+        assertThat(new CurrencyValidator().isValid("ABC", null)).isFalse();
+    }
+
+    @Test
+    public void shouldBeValidForExistingCurrencyCode() {
+        assertThat(new CurrencyValidator().isValid("USD", null)).isTrue();
+    }
+}

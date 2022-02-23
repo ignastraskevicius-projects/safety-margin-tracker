9a2280674bccea54e1e789ac12821871f2d7d4c6
Ignas Traskevicius <ignas.traskevicius@gmail.com>
2022-02-23 23:44:37 +0100

    multiple javax.validation errors can now be exposed to the client for the same field

--- src/main/java/org/ignast/stockinvesting/api/controller/FieldValidationSequence.java
+++ src/main/java/org/ignast/stockinvesting/api/controller/FieldValidationSequence.java
@@ -3,11 +3,8 @@ package org.ignast.stockinvesting.api.controller;
 import javax.validation.GroupSequence;
 import javax.validation.groups.Default;
 
-@GroupSequence({ Default.class, AllowedCharacters.class, AdherenceToStandard.class })
+@GroupSequence({ Default.class, AdherenceToStandard.class })
 public interface FieldValidationSequence {
-}
-
-interface AllowedCharacters {
 }
 
 interface AdherenceToStandard {

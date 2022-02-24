69717b94f7a22b7ca4ab5fe1a22bf3070b4f334a
Ignas Traskevicius <ignas.traskevicius@gmail.com>
2022-02-24 19:06:55 +0100

    domain company has a name

--- src/test/java/org/ignast/stockinvesting/domain/CompanyTest.java
+++ src/test/java/org/ignast/stockinvesting/domain/CompanyTest.java
@@ -21,28 +21,52 @@ class CompanyTest {
     @ValueSource(strings = { "EUR", "USD" })
     public void shouldHaveFunctionalCurrency(String currencyCode) {
         val currency = Currency.getInstance(currencyCode);
-        assertThat(new Company("US", currency).getFunctionalCurrency()).isEqualTo(currency);
+        assertThat(new Company("Amazon", "US", currency).getFunctionalCurrency()).isEqualTo(currency);
     }
 
     @Test
     public void shouldNotHaveNullFunctionalCurrency() {
-        assertThatExceptionOfType(NullPointerException.class).isThrownBy(() -> new Company("US", null));
+        assertThatExceptionOfType(NullPointerException.class).isThrownBy(() -> new Company("Amazon", "US", null));
     }
 
     @ParameterizedTest
     @ValueSource(strings = { "US", "ES" })
     public void shouldHaveHomeCountry(String countryCode) {
-        assertThat(new Company(countryCode, usd()).getHomeCountry()).isEqualTo(countryCode);
+        assertThat(new Company("Amazon", countryCode, usd()).getHomeCountry()).isEqualTo(countryCode);
     }
 
     @Test
     public void companyShouldHaveNullCountry() {
-        assertThatExceptionOfType(NullPointerException.class).isThrownBy(() -> new Company(null, usd()));
+        assertThatExceptionOfType(NullPointerException.class).isThrownBy(() -> new Company("Amazon", null, usd()));
     }
 
     @Test
     public void companyShouldHaveInvalidCountryCode() {
-        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new Company("AB", usd()));
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new Company("Amazon", "AB", usd()));
+    }
+
+    @Test
+    public void shouldHaveNeitherTooLongNorEmptyName() {
+        assertThat(new Company("Amazon", "US", usd()).getName()).isEqualTo("Amazon");
+    }
+
+    @Test
+    public void companyShouldHaveNullName() {
+        assertThatExceptionOfType(NullPointerException.class).isThrownBy(() -> new Company(null, "US", usd()));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = { 1, 255 })
+    public void shouldHaveNeitherTooLongNorEmptyName(int boundaryValidLength) {
+        val name = "c".repeat(boundaryValidLength);
+        assertThat(new Company(name, "US", usd()).getName()).isEqualTo(name);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = { 0, 256 })
+    public void companyNameShouldNotBeEmpty(int boundaryInvalidLength) {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> new Company("c".repeat(boundaryInvalidLength), "US", usd()));
     }
 
     private Currency usd() {
